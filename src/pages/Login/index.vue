926ab46021ef2375ed2940899381045edb96d596
hqw567 <hqw6@qq.com>
2022-01-09 12:29:21 +0800

    1/9

---
+++ src/pages/Login/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+我是登陆页
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+    computed: {
+
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    watch: {
+
+    },
+    methods: {
+
+    },
+    components: {
+
+    },
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/pages/Search/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        我是搜索页
+        <h1>query参数：{{ $route.query.k }}</h1>
+        <h1>params{{ $route.params.key }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+    computed: {
+
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    watch: {
+
+    },
+    methods: {
+
+    },
+    components: {
+
+    },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
